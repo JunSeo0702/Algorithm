64ae11a5cb0b779902257c939e0788dbfac6e536
JunSeo0702 <144515239+JunSeo0702@users.noreply.github.com>
2024-11-25 12:02:56 +0900

    [level 0] Title: 특수문자 출력하기, Time: 126.96 ms, Memory: 62.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String st = "!@#$%^&*(\\'\"<>?:;";
+    
+        System.out.print(st);
+    }
+}
